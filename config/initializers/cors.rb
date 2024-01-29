453d7f51283769224923e175d223c29de9736206
alvinlearner <alvin47k@outlook.com>
2024-01-29 18:06:41 +0300

    reconfigure origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 #config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://main--keen-horse-867dfe.netlify.app/'
+      origins 'https://main--keen-horse-867dfe.netlify.app'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   
